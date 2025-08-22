44c98484250058468052a6ccde4fcb7ec3ab56c7
eshwarthath <eshwarthath@gmail.com>
2025-08-22 15:26:17 +0530

    Create sort_words_alphabetically.py

---
+++ sort_words_alphabetically.py
@@ -0,0 +1,18 @@
+sentence = input("Enter a sentence: ").lower()
+words = sentence.split()
+result_list = []
+words_sorting = sorted(words)
+result = " ".join(words_sorting)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
