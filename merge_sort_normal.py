f65a7cc37b600c1059fb53460a2884c7026a5aac
eshwarthath <eshwarthath@gmail.com>
2025-09-07 00:10:52 +0530

    Create merge_sort_normal.py

---
+++ merge_sort_normal.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        i = j = k = 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+                k +=1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+                k +=1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k +=1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k +=1
+
+n = int(input("Enter the number of elements to be stored in the list: "))
+
+arr = []
+
+for i in range(n):
+    num = int(input(f"Enter number {i+1}: "))
+    arr.append(num)
+
+print(f"original list: ",arr)
+
+merge_sort(arr)
+
+print(f"sorted list: ",arr)
