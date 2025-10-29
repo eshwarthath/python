0cf00dbc7dc7f6a53e812c25600b3e27cd696867
eshwarthath <eshwarthath@gmail.com>
2025-10-30 00:32:32 +0530

    Implement formatted number printing function
    Add a function to print numbers in different formats

---
+++ string_formatting.py
@@ -0,0 +1,14 @@
+def print_formatted(number):
+    width = len(bin(number)[2:])   # length of binary representation of the max number
+    for i in range(1, number + 1):
+        deci = str(i).rjust(width)
+        octa = oct(i)[2:].rjust(width)
+        hexa = hex(i)[2:].upper().rjust(width)
+        bina = bin(i)[2:].rjust(width)
+        print(deci, octa, hexa, bina)
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
